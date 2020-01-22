52d0f7060d845fcb4b093fb65af93b6c2e8d4e38
davide <davide.mastroianni@designcoaching.net>
2020-01-22 16:34:44 +0100

    aggiunta classe main

---
+++ src/main/java/it.md.ofp/OldFashionPound.java
@@ -0,0 +1,16 @@
+package it.md.ofp;
+
+public class OldFashionPound {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        // parse input
+
+        // create penny objects
+
+        // perform operation
+
+        // return result converting pennies into a pound shelly penny (pound shelly penny) denomination
+    }
+
+}
