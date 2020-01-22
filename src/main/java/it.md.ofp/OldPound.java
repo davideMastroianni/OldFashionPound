0d997c2af39ff6c50a98733c83ed9fc490982615
davide-pc <mastroianni.davidedst@gmail.com>
2020-01-23 00:01:52 +0100

    Aggiunti attributi per i remainder

--- src/main/java/it.md.ofp/OldPound.java
+++ src/main/java/it.md.ofp/OldPound.java
@@ -14,15 +14,34 @@ public class OldPound {
     private int shillings;
     @Setter @Getter
     private int pence;
+    @Setter @Getter
+    private int remainderPounds;
+    @Setter @Getter
+    private int remainderShillings;
+    @Setter @Getter
+    private int remainderPence;
 
     public OldPound(int pounds, int shillings, int pence) {
         this.pounds = pounds;
         this.shillings = shillings;
         this.pence = pence;
+        this.remainderPounds = 0;
+        this.remainderShillings = 0;
+        this.remainderPence = 0;
     }
 
     @Override
     public String toString() {
-        return pounds + "p " + shillings + "s " + pence + "d";
+        if (remainderPence == 0 && remainderShillings == 0 && remainderPence == 0) {
+            return pounds + "p " + shillings + "s " + pence + "d";
+        } else {
+            return pounds + "p " + shillings + "s " + pence + "d" + " (-" + remainderPence + "p -" + remainderShillings + "s -" + remainderPence + "d)";
+        }
+    }
+
+    public void setRemainders(int remainderPounds, int remainderShillings, int remainderPence) {
+        this.remainderPounds = remainderPounds;
+        this.remainderShillings = remainderShillings;
+        this.remainderPence = remainderPence;
     }
 }
