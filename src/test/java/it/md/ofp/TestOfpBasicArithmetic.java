036c2f3c5fd892dc15642e53927a6c33b3cb214e
davide-pc <mastroianni.davidedst@gmail.com>
2020-01-23 00:01:19 +0100

    test per la classe diff

--- src/test/java/it/md/ofp/TestOfpBasicArithmetic.java
+++ src/test/java/it/md/ofp/TestOfpBasicArithmetic.java
@@ -34,4 +34,53 @@ public class TestOfpBasicArithmetic {
         assertThat(result.getShillings()).isEqualTo(0);
         assertThat(result.getPence()).isEqualTo(0);
     }
+
+    @Test
+    public void diff_calculate_shouldReturnCorrectlyChangedOldPound_withNoNegative() {
+        // SETUP
+        Diff sut = new Diff();
+        OldPound op_1 = new OldPound(5, 17 ,8);
+        OldPound op_2 = new OldPound(3, 4 ,8);
+        // ACT
+        OldPound result = sut.calculate(op_1, op_2);
+        // VERIFY
+        assertThat(result).isNotNull();
+        assertThat(result.getPounds()).isEqualTo(2);
+        assertThat(result.getShillings()).isEqualTo(13);
+        assertThat(result.getPence()).isEqualTo(0);
+    }
+
+    @Test
+    public void diff_calculate_shouldReturnCorrectlyChangedOldPound_withPenceNegative() {
+        // SETUP
+        Diff sut = new Diff();
+        OldPound op_1 = new OldPound(5, 17 ,8);
+        OldPound op_2 = new OldPound(3, 4 ,16);
+        // ACT
+        OldPound result = sut.calculate(op_1, op_2);
+        // VERIFY
+        assertThat(result).isNotNull();
+        assertThat(result.getPounds()).isEqualTo(2);
+        assertThat(result.getShillings()).isEqualTo(13);
+        assertThat(result.getPence()).isEqualTo(0);
+        assertThat(result.getRemainderPence()).isEqualTo(8);
+    }
+
+    @Test
+    public void diff_calculate_shouldReturnCorrectlyChangedOldPound_withSomeNegative() {
+        // SETUP
+        Diff sut = new Diff();
+        OldPound op_1 = new OldPound(5, 17 ,8);
+        OldPound op_2 = new OldPound(8, 4 ,16);
+        // ACT
+        OldPound result = sut.calculate(op_1, op_2);
+        // VERIFY
+        assertThat(result).isNotNull();
+        assertThat(result.getPounds()).isEqualTo(0);
+        assertThat(result.getShillings()).isEqualTo(0);
+        assertThat(result.getPence()).isEqualTo(0);
+        assertThat(result.getRemainderPence()).isEqualTo(8);
+        assertThat(result.getRemainderShillings()).isEqualTo(7);
+        assertThat(result.getRemainderPounds()).isEqualTo(2);
+    }
 }
