91ff0c9a270204beafa4af909046bdfb81ad9667
davide-pc <mastroianni.davidedst@gmail.com>
2020-01-22 23:16:28 +0100

    Composto metodo main con il flusso dell'algoritmo per risolvere il calcolo partendo dal dato grezzo fino al risultato

--- src/main/java/it.md.ofp/OldFashionPound.java
+++ src/main/java/it.md.ofp/OldFashionPound.java
@@ -3,14 +3,21 @@ package it.md.ofp;
 public class OldFashionPound {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-        // parse input
-
-        // create penny objects
-
-        // perform operation
-
-        // return result converting pennies into a pound shelly penny (pound shelly penny) denomination
+        try {
+            // PARSE
+            RawOperationParser parser = new RawOperationParser(args);
+            OldPound op_1 = parser.parseLeftPound();
+            OldPound op_2 = parser.parseRightPound();
+            OfpArithmeticEnum operator = parser.parseOperator();
+            // SETUP OPERATION
+            OfpBasicArithmetic basicArithmetic = BasicArithmeticFactory.getBasicArithmetic(operator);
+            OfpOperation operation = new OfpOperation(op_1, op_2, basicArithmetic);
+            // CALCULATE
+            OldPound result = operation.calculate();
+            System.out.println(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
