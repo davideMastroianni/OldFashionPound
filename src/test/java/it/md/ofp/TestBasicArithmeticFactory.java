4b8b949bc04070b431ec4a78f1a69abb943db0bf
davide-pc <mastroianni.davidedst@gmail.com>
2020-01-22 23:14:53 +0100

    Test della factory

---
+++ src/test/java/it/md/ofp/TestBasicArithmeticFactory.java
@@ -0,0 +1,35 @@
+package it.md.ofp;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class TestBasicArithmeticFactory {
+
+    @Test
+    public void getBasicArithmetic_shouldReturnSumObject_withSumEnum() {
+        // SETUP
+        OfpArithmeticEnum operator = OfpArithmeticEnum.SUM;
+        // ACT
+        try {
+            OfpBasicArithmetic basicArithmetic = BasicArithmeticFactory.getBasicArithmetic(operator);
+            // VERIFY
+            assertThat(basicArithmetic).isNotNull();
+            assertThat(basicArithmetic).isInstanceOf(Sum.class);
+        } catch (Exception e) {
+            fail("Should not throw exception with Enum %s", operator);
+        }
+    }
+
+    @Test
+    public void getBasicArithmetic_shouldThrowException_withWrongEnum() {
+        // SETUP
+        OfpArithmeticEnum operator = mock(OfpArithmeticEnum.class);
+        // ACT & VERIFY
+        assertThatThrownBy(() -> {
+            OfpBasicArithmetic basicArithmetic = BasicArithmeticFactory.getBasicArithmetic(operator);
+        }).isInstanceOf(Exception.class).hasMessage("Basic Arithmetic not recognized");
+    }
+
+}
