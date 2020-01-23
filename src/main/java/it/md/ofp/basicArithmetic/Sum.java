51a1fc4d6d1c8d635c8e8db0b28f680908cecc30
davide-pc <mastroianni.davidedst@gmail.com>
2020-01-23 21:40:12 +0100

    Delegata l'ottimizzazione dei valori all'oggetto OldPound

--- src/main/java/it/md/ofp/basicArithmetic/Sum.java
+++ src/main/java/it/md/ofp/basicArithmetic/Sum.java
@@ -17,16 +17,11 @@ public class Sum implements OfpBasicArithmetic {
 
     @Override
     public OldPound calculate() {
-        // calculate pence
         int pence = this.op_1.getPence() + this.op_2.getPence();
-        int actualPence = pence % 12;
-        int penceToShillings = (pence - actualPence) / 12;
-        // calculate shillings
-        int shillings = this.op_1.getShillings() + this.op_2.getShillings() + penceToShillings;
-        int actualShillings = shillings % 20;
-        int shillingsToPounds = (shillings - actualShillings) / 20;
-        // calculate pounds
-        int pounds = this.op_1.getPounds() + this.op_2.getPounds() + shillingsToPounds;
-        return new OldPound(pounds, actualShillings, actualPence);
+        int shillings = this.op_1.getShillings() + this.op_2.getShillings();
+        int pounds = this.op_1.getPounds() + this.op_2.getPounds();
+        OldPound result = new OldPound(pounds, shillings, pence);
+        result.optimizeValues();
+        return result;
     }
 }
