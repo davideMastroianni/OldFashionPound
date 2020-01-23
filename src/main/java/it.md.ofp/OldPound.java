effa6cf4fb8e413b96ae8c55bc36bd33b8e1e6ba
davide <davide.mastroianni@designcoaching.net>
2020-01-23 16:32:14 +0100

    Aggiunto metodo per la conversione dei valori. Aggiunto metodo per determinare se un oldPound è empty Migliorato il toString Il remainder ora è un oggetto oldPound

--- src/main/java/it.md.ofp/OldPound.java
+++ src/main/java/it.md.ofp/OldPound.java
@@ -8,40 +8,50 @@ import lombok.Setter;
  */
 public class OldPound {
 
-    @Setter @Getter
+    @Getter
     private int pounds;
-    @Setter @Getter
+    @Getter
     private int shillings;
-    @Setter @Getter
+    @Getter
     private int pence;
     @Setter @Getter
-    private int remainderPounds;
-    @Setter @Getter
-    private int remainderShillings;
-    @Setter @Getter
-    private int remainderPence;
+    private OldPound remainder;
 
     public OldPound(int pounds, int shillings, int pence) {
         this.pounds = pounds;
         this.shillings = shillings;
         this.pence = pence;
-        this.remainderPounds = 0;
-        this.remainderShillings = 0;
-        this.remainderPence = 0;
     }
 
     @Override
     public String toString() {
-        if (remainderPence == 0 && remainderShillings == 0 && remainderPence == 0) {
-            return pounds + "p " + shillings + "s " + pence + "d";
+        String moneyTalk;
+        if (this.pounds > 0) {
+            moneyTalk = pounds + "p " + shillings + "s " + pence + "d";
+        } else if (this.shillings > 0) {
+            moneyTalk = shillings + "s " + pence + "d";
         } else {
-            return pounds + "p " + shillings + "s " + pence + "d" + " (-" + remainderPence + "p -" + remainderShillings + "s -" + remainderPence + "d)";
+            moneyTalk = pence + "d";
         }
+        if (remainder != null) {
+            moneyTalk += " (" + remainder.toString() + ")";
+        }
+        return moneyTalk;
     }
 
-    public void setRemainders(int remainderPounds, int remainderShillings, int remainderPence) {
-        this.remainderPounds = remainderPounds;
-        this.remainderShillings = remainderShillings;
-        this.remainderPence = remainderPence;
+    public boolean isEmpty() {
+        return this.pounds == 0 && this.shillings == 0 && this.pence == 0;
+    }
+
+    public void optimizeValues() {
+        int actualPence = this.pence % 12;
+        int penceToShillings = this.pence / 12;
+        int actualShillings = (this.shillings + penceToShillings) % 20;
+        int shillingsToPounds = (this.shillings + penceToShillings) / 20;
+        int actualPounds = this.pounds + shillingsToPounds;
+        // SET ATTRIBUTES
+        this.pence = actualPence;
+        this.shillings = actualShillings;
+        this.pounds = actualPounds;
     }
 }
