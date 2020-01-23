00915819bff8342170c7b096965d6e069f7fb813
davide-pc <mastroianni.davidedst@gmail.com>
2020-01-23 18:58:33 +0100

    Cambiato il messaggio di errore.

--- src/main/java/it/md/ofp/basicArithmetic/BasicArithmeticFactory.java
+++ src/main/java/it/md/ofp/basicArithmetic/BasicArithmeticFactory.java
@@ -6,11 +6,11 @@ public class BasicArithmeticFactory {
 
     public static OfpBasicArithmetic getBasicArithmetic(OfpArithmeticEnum oae, OldPound op_1, OldPound op_2) throws Exception {
         if (oae == OfpArithmeticEnum.SUM) {
-                return new Sum(op_1, op_2);
+            return new Sum(op_1, op_2);
         } else if (oae == OfpArithmeticEnum.DIFF) {
             return new Diff(op_1, op_2);
         } else {
-            throw new Exception("Basic Arithmetic not recognized");
+            throw new Exception("Operator MUL and DIV MUST have left term of type OldPound and right term of type int");
         }
     }
 
@@ -20,7 +20,7 @@ public class BasicArithmeticFactory {
         } else if (oae == OfpArithmeticEnum.DIV) {
             return new Div(op_1, op_2);
         } else  {
-            throw new Exception("Basic Arithmetic not recognized");
+            throw new Exception("Operator SUM and DIFF want both terms to be type OldPound");
         }
     }
 }
