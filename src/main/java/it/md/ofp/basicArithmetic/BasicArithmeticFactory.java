51771478bc7c571b31c1dc6a3cbd524f1fd2779b
davide <davide.mastroianni@designcoaching.net>
2020-01-23 16:34:42 +0100

    Overloading del metodo di Factory per gestire i due possibili tipi di input delle operazioni

---
+++ src/main/java/it/md/ofp/basicArithmetic/BasicArithmeticFactory.java
@@ -0,0 +1,26 @@
+package it.md.ofp.basicArithmetic;
+
+import it.md.ofp.OldPound;
+
+public class BasicArithmeticFactory {
+
+    public static OfpBasicArithmetic getBasicArithmetic(OfpArithmeticEnum oae, OldPound op_1, OldPound op_2) throws Exception {
+        if (oae == OfpArithmeticEnum.SUM) {
+                return new Sum(op_1, op_2);
+        } else if (oae == OfpArithmeticEnum.DIFF) {
+            return new Diff(op_1, op_2);
+        } else {
+            throw new Exception("Basic Arithmetic not recognized");
+        }
+    }
+
+    public static OfpBasicArithmetic getBasicArithmetic(OfpArithmeticEnum oae, OldPound op_1, int op_2) throws Exception {
+        if (oae == OfpArithmeticEnum.MUL) {
+            return new Mul(op_1, op_2);
+        } else if (oae == OfpArithmeticEnum.DIV) {
+            return new Div(op_1, op_2);
+        } else  {
+            throw new Exception("Basic Arithmetic not recognized");
+        }
+    }
+}
