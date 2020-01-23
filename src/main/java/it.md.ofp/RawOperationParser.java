ccf69c0203970cd46d255f5edd5d12b49da972a6
davide <davide.mastroianni@designcoaching.net>
2020-01-23 16:36:47 +0100

    Refactor logica parser

--- src/main/java/it.md.ofp/RawOperationParser.java
+++ src/main/java/it.md.ofp/RawOperationParser.java
@@ -1,54 +1,92 @@
 package it.md.ofp;
 
+import it.md.ofp.basicArithmetic.OfpArithmeticEnum;
 import lombok.Getter;
 
 class RawOperationParser {
 
     @Getter private String[] rawOperation;
+    @Getter private OldPound leftOperation;
+    @Getter private OldPound rightOperationPound;
+    @Getter private OfpArithmeticEnum operator;
+    @Getter private int rightOperationInteger;
 
-    public RawOperationParser(String[] rawOperation) {
+    RawOperationParser(String[] rawOperation) {
         this.rawOperation = rawOperation;
     }
 
-    public OfpArithmeticEnum parseOperator() throws Exception {
-        String rawOperator = rawOperation[3];
-        switch (rawOperator) {
-            case "+":
-                return OfpArithmeticEnum.SUM;
-            case "-":
-                return OfpArithmeticEnum.DIFF;
-            case "*":
-                return OfpArithmeticEnum.MUL;
-            case "/":
-                return OfpArithmeticEnum.DIV;
-            default:
-                throw new Exception("Malformed raw operation.");
+    void parse() throws Exception {
+        validateRawOperationLength();
+        parseOperator();
+        validateRawOperationType(this.operator);
+        parseLeftOperation();
+        parseRightOperation(this.operator);
+    }
+
+    private void validateRawOperationLength() throws Exception {
+        if (!(rawOperation.length == 5 || rawOperation.length == 7)) {
+            throw new Exception("Raw operation is malformed. It's required to be in this form \"Xp Ys Zd [*|/] K\" or \"Xp Ys Zd [+|-] Ap Bs Cd\"");
         }
     }
 
-    public OldPound parseLeftPound() {
+    private void parseOperator() throws Exception {
+        String rawOperator = rawOperation[3];
+        if (rawOperator.equals("+")) {
+            this.operator = OfpArithmeticEnum.SUM;
+        } else if (rawOperator.equals("-")) {
+            this.operator = OfpArithmeticEnum.DIFF;
+        } else if (rawOperator.equals("*")) {
+            this.operator = OfpArithmeticEnum.MUL;
+        } else if (rawOperator.equals("/")) {
+            this.operator = OfpArithmeticEnum.DIV;
+        } else {
+            throw new Exception("Malformed expression: operator " + rawOperator + " is not supported.");
+        }
+    }
+
+    private void validateRawOperationType(OfpArithmeticEnum operator) throws Exception {
+        if (operator == OfpArithmeticEnum.SUM || operator == OfpArithmeticEnum.DIFF) {
+            if (rawOperation.length != 7) {
+                throw new Exception("Raw operation is malformed for operator / and *. It required to be in this form \"Xp Ys Zd [+|-] Ap Bs Cd\"");
+            }
+        } else {
+            if (rawOperation.length != 5) {
+                throw new Exception("Raw operation is malformed for operator / and *. It required to be in this form \"Xp Ys Zd [*|/] K\"");
+            }
+        }
+    }
+
+    private void parseLeftOperation() {
         String rawPounds = rawOperation[0];
         String rawShillings = rawOperation[1];
         String rawPence = rawOperation[2];
-        return parseOldPound(rawPounds, rawShillings, rawPence);
+        this.leftOperation = parseOldPound(rawPounds, rawShillings, rawPence);
     }
 
-    public OldPound parseRightPound() {
-        String rawPounds = rawOperation[4];
-        String rawShillings = rawOperation[5];
-        String rawPence = rawOperation[6];
-        return parseOldPound(rawPounds, rawShillings, rawPence);
+    private void parseRightOperation(OfpArithmeticEnum operator) {
+        if (operator == OfpArithmeticEnum.SUM || operator == OfpArithmeticEnum.DIFF) {
+            String rawPounds = rawOperation[4];
+            String rawShillings = rawOperation[5];
+            String rawPence = rawOperation[6];
+            this.rightOperationPound = parseOldPound(rawPounds, rawShillings, rawPence);
+        } else {
+            String rawInt = rawOperation[4];
+            this.rightOperationInteger = Integer.parseInt(rawInt);
+        }
+
     }
 
     private OldPound parseOldPound(String rawPounds, String rawShillings, String rawPence) {
-        int pounds = rawStringToInt(rawPounds);
-        int shillings = rawStringToInt(rawShillings);
-        int pence = rawStringToInt(rawPence);
+        int pounds = rawValueToInt(rawPounds);
+        int shillings = rawValueToInt(rawShillings);
+        int pence = rawValueToInt(rawPence);
         return new OldPound(pounds, shillings, pence);
     }
 
-    private int rawStringToInt(String rawString) {
+    private int rawValueToInt(String rawString) {
         String stringInt = rawString.substring(0, rawString.length() - 1);
         return Integer.parseInt(stringInt);
     }
+
+
 }
